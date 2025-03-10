3131bf8ab654c07ef702602695680796022546ce
Amos Reginald Jr <amosjr08@aol.com>
2025-03-10 11:13:55 -0400

    added two buttons, 1) °f to °c and vice versa 2) added a button to flip the gradient

---
+++ CircleButtons.swift
@@ -0,0 +1,8 @@
+//
+//  CircleButtons.swift
+//  weather_app
+//
+//  Created by Amos Reginald Jr. on 3/6/25.
+//
+
+import Foundation
---
+++ weather_app/FahrenheitToCelsiusButton.swift
@@ -0,0 +1,8 @@
+//
+//  FahrenheitToCelsiusButton.swift
+//  weather_app
+//
+//  Created by Amos Reginald Jr. on 3/6/25.
+//
+
+import Foundation
